337aa041b9e02ce59d7a0e223308c85f9e83c8e8
ZooBaer404 <zubairstudytech@gmail.com>
2025-06-04 15:46:16 +0430

    config: migrate configurations to org

---
+++ custom.el
@@ -0,0 +1,29 @@
+;;; custom.el --- Custom Emacs configuration inserted by Emacs itself  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2025  zoobaer
+
+;; Author: zoobaer <zoobaer@fedora>
+;; Keywords: lisp
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ad-redefinition-action 'accept)
+ '(backup-directory-alist `(("." \, (concat user-emacs-directory "backups"))))
+ '(confirm-kill-processes nil)
+ '(custom-safe-themes
+   '("fffef514346b2a43900e1c7ea2bc7d84cbdd4aa66c1b51946aade4b8d343b55a"
+     default))
+ '(indent-tabs-mode nil)
+ '(kill-whole-line t)
+ '(load-prefer-newer t t)
+ '(mouse-yank-at-point t)
+ '(read-buffer-completion-ignore-case t)
+ '(read-file-name-completion-ignore-case t)
+ '(show-trailing-whitespace nil)
+ '(use-package-always-defer t)
+ '(use-package-verbose nil))
+
+(provide 'custom)
